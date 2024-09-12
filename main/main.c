adc008cb95d0710f718660f29710e2cfd882f9a5
Kanyakorn <65030018@kmitl.ac.th>
2024-09-12 13:50:58 +0700

    lab2

--- main/main.c
+++ main/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 
 #include "freertos/FreeRTOS.h"
@@ -7,18 +7,24 @@
 
 TaskHandle_t MyFirstTaskHandle = NULL;
 
-void My_First_Task(void* arg)
+void My_First_Task(void * arg)
 {
-    uint16_t i = 0;
-    while(1)
-    {
-        printf("Hello My First Task %d\n", i);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-        i++;
-    }
+	uint16_t i = 0;
+	while(1)
+	{
+		printf("Hello My First Task %d\n",i);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+		i++;
+	}
 }
 
 void app_main(void)
 {
-    xTaskCreate(My_First_Task, "First_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+	xTaskCreate(My_First_Task, "Fitst_Task", 4096, NULL, 10, &MyFirstTaskHandle);
+
 }
+
